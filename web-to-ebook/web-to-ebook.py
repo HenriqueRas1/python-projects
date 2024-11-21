25fbce8fcc7af218b6bd05f17edc15d91951279c
HenriqueRas1 <henrique.ras@hotmail.com>
2024-11-21 13:02:45 -0300

    feat: add the web requisition and parsing HTML

--- web-to-ebook/web-to-ebook.py
+++ web-to-ebook/web-to-ebook.py
@@ -1,3 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from ebooklib import epub
+
+def create_ebook(url, book_title):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
